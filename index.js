503d90defc315daac2f17a2c46300899ebcc08df
Gehrig Barnes <gehrigbarnes@gmail.com>
2021-12-16 11:47:16 -0600

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,46 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+function titleCased() {
+  return arrayUpper;
 }
+
+console.log(titleCased);
+
+
+function splitSentence (tut) {
+  const newArray = []
+  for(let i = 0; i < tutorials.length; i++) {
+    newArray.push(tut[i].split(' '))
+  }
+  return newArray;
+}
+
+const arraySplit = splitSentence(tutorials);
+
+function upperCase (upper) {
+  let newArray = []
+  for(let i = 0; i < arraySplit.length; i++) {
+    newArray.push(upper[i].map(x => x.charAt(0).toUpperCase() + x.slice(1)).join(' '))
+
+  }
+  return newArray
+}
+
+const arrayUpper = upperCase (arraySplit);
+
+
+// const arrayInArray = arraySplit[0];
+
+// const uppercase = arrayInArray.map(x => x.charAt(0).toUpperCase() + x.slice(1))
+// console.log(uppercase);
+
+
+
+
+// const word = "hello"
+
+// const word2 = word.charAt(0).toUpperCase() + word.slice(1);
+
+// console.log(word2);
